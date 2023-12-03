48a9e4cdb48a31f7a2cb5e4c0c7807b5358cfdc9
kensiiwasaki <kensiiwasaki@gmail.com>
2023-12-04 00:00:33 +0900

    ボタン調整

--- app/routes/x-profile-clone.tsx
+++ app/routes/x-profile-clone.tsx
@@ -3,37 +3,37 @@ export default function Page() {
     <div className="flex justify-around">
       <section className="flex flex-col items-start gap-3 text-lg">
         <button className="px-2 py-1 rounded-full hover:bg-gray-200">X</button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           ホーム
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           話題を検索
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           通知
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           メッセージ
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           リスト
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           ブックマーク
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           コミュニティ
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           プレミアム
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           プロフィール
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           もっと見る
         </button>
-        <button className="px-2 py-1 rounded-full hover:bg-gray-200">
+        <button className="px-3 py-2 rounded-full hover:bg-gray-200">
           ポストする
         </button>
       </section>
