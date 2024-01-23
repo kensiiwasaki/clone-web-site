d0ec5dae45be0a0793c477d3827e749d258fa712
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-23 22:58:30 +0900

    1の部分を再現

--- app/routes/challenge-1.tsx
+++ app/routes/challenge-1.tsx
@@ -5,7 +5,7 @@ const ChallengeComponent = () => {
     <div className="absolute top-1/2 left-1/2 w-96 h-96 mt-[-200px] ml-[-200px] rounded-sm bg-gradient-to-tr from-[#43389F] to-[#4ec6ca] text-white font-mono antialiased">
       <div className="absolute top-1/2 left-1/2 transform-[-50%,-50%]">
         <div className="relative h-24 w-48">
-          <div className="absolute z-10 top-0 left-[-4rem] h-10 w-5 bg-white rounded-sm shadow-md"></div>
+          <div className="absolute z-10 top-0 left-[-16px] h-10 w-5 bg-white rounded-sm shadow-md"></div>
           <div className="absolute z-20 top-0 left-0 h-24 w-6 bg-white rounded-sm shadow-md"></div>
           <div className="absolute z-10 top-0 left-4 box-border h-24 w-24 rounded-full border-6 border-white shadow-md"></div>
           <div className="absolute z-10 top-0 left-24 box-border h-24 w-24 rounded-full border-6 border-white shadow-md"></div>
