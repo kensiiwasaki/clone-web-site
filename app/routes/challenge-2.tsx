ab65e62e0d2341dfcda96f9dffc9daa9eb442793
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-28 18:46:09 +0900

    指定方法ミスってた

--- app/routes/challenge-2.tsx
+++ app/routes/challenge-2.tsx
@@ -8,31 +8,29 @@ const ChallengeComponent = () => {
   };
 
   return (
-    <div className="h-100 w-100">
-      <div className="fixed inset-0 flex justify-center items-center bg-green-500">
-        <div className="relative">
+    <div className="h-[500px] w-[500px] fixed inset-0 flex justify-center items-center bg-green-500">
+      <div className="relative">
+        <div
+          className={`menu-icon cursor-pointer z-50 w-20 ${
+            isActive ? "active" : ""
+          }`}
+          onClick={toggleMenuIcon}
+        >
           <div
-            className={`menu-icon cursor-pointer z-50 w-20 ${
-              isActive ? "active" : ""
+            className={`h-2 w-full bg-white rounded-md shadow ${
+              isActive ? "animate-line1" : ""
             }`}
-            onClick={toggleMenuIcon}
-          >
-            <div
-              className={`h-2 w-full bg-white rounded-md shadow ${
-                isActive ? "animate-line1" : ""
-              }`}
-            ></div>
-            <div
-              className={`h-2 w-full bg-white rounded-md shadow my-2 ${
-                isActive ? "animate-line2" : ""
-              }`}
-            ></div>
-            <div
-              className={`h-2 w-full bg-white rounded-md shadow ${
-                isActive ? "animate-line3" : ""
-              }`}
-            ></div>
-          </div>
+          ></div>
+          <div
+            className={`h-2 w-full bg-white rounded-md shadow my-2 ${
+              isActive ? "animate-line2" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-2 w-full bg-white rounded-md shadow ${
+              isActive ? "animate-line3" : ""
+            }`}
+          ></div>
         </div>
       </div>
     </div>
