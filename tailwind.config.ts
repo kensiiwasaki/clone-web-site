ff47bdba97dfe61f8cab2e4e9b4f538f1a1c92ae
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-28 18:35:28 +0900

    animation用の設定追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,25 @@ export default {
       fontFamily: {
         courier: ["Courier New", "Courier", "sans-serif"],
       },
+      keyframes: {
+        line1: {
+          "0%, 50%": { transform: "translateY(0) rotate(0deg)" },
+          "100%": { transform: "translateY(1rem) rotate(45deg)" },
+        },
+        line2: {
+          "0%": { transform: "scale(1)", opacity: "1" },
+          "100%": { transform: "scale(0)", opacity: "0" },
+        },
+        line3: {
+          "0%, 50%": { transform: "translateY(0) rotate(0deg)" },
+          "100%": { transform: "translateY(-1rem) rotate(-45deg)" },
+        },
+      },
+      animation: {
+        line1: "line1 0.7s ease-in-out forwards",
+        line2: "line2 0.7s ease-in-out forwards",
+        line3: "line3 0.7s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
