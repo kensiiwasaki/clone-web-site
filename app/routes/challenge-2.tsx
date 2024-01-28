b6ba86710a342e3574cabeec1df2fae316e12425
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-28 18:32:25 +0900

    challenge-2

---
+++ app/routes/challenge-2.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const ChallengeComponent = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenuIcon = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-green-500">
+      <div className="relative">
+        <div
+          className={`menu-icon cursor-pointer z-50 w-20 ${
+            isActive ? "active" : ""
+          }`}
+          onClick={toggleMenuIcon}
+        >
+          <div
+            className={`h-2 w-full bg-white rounded-md shadow transition-all duration-200 ease-in-out ${
+              isActive ? "transform rotate-45 translate-y-2.5" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-2 w-full bg-white rounded-md shadow transition-all duration-200 ease-in-out my-2 ${
+              isActive ? "opacity-0" : ""
+            }`}
+          ></div>
+          <div
+            className={`h-2 w-full bg-white rounded-md shadow transition-all duration-200 ease-in-out ${
+              isActive ? "transform -rotate-45 -translate-y-2.5" : ""
+            }`}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChallengeComponent;
