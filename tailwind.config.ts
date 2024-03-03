854cd7cc5a3e17e8d8e28be7ef57dbed77e6f6c5
kensiiwasaki <kensiiwasaki@gmail.com>
2024-03-03 22:44:55 +0900

    animation追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ export default {
         courier: ["Courier New", "Courier", "sans-serif"],
       },
       keyframes: {
+        // challenge 2
         line1: {
           "0%, 50%": { transform: "translateY(0) rotate(0deg)" },
           "100%": { transform: "translateY(1rem) rotate(45deg)" },
@@ -20,6 +21,8 @@ export default {
           "0%, 50%": { transform: "translateY(0) rotate(0deg)" },
           "100%": { transform: "translateY(-1rem) rotate(-45deg)" },
         },
+
+        // challenge 3
         "sky-turns-black": {
           "0%, 100%": { backgroundColor: "#272C34" },
           "30%, 70%": { backgroundColor: "#7DDFFC" },
@@ -49,11 +52,46 @@ export default {
           "0%, 100%": { backgroundColor: "#272C34" },
           "30%, 70%": { backgroundColor: "#F0DE75" },
         },
+
+        // challenge 4
+        "jump-jump-1": {
+          "0%, 70%": {
+            boxShadow: "2px 2px 3px 2px rgba(0,0,0,0.2)",
+            transform: "scale(0)",
+          },
+          "100%": {
+            boxShadow: "10px 10px 15px 0 rgba(0,0,0,0.3)",
+            transform: "scale(1)",
+          },
+        },
+        "jump-jump-2": {
+          "0%, 40%": {
+            boxShadow: "2px 2px 3px 2px rgba(0,0,0,0.2)",
+            transform: "scale(0)",
+          },
+          "100%": {
+            boxShadow: "10px 10px 15px 0 rgba(0,0,0,0.3)",
+            transform: "scale(1)",
+          },
+        },
+        "jump-jump-3": {
+          "0%, 10%": {
+            boxShadow: "2px 2px 3px 2px rgba(0,0,0,0.2)",
+            transform: "scale(0)",
+          },
+          "100%": {
+            boxShadow: "10px 10px 15px 0 rgba(0,0,0,0.3)",
+            transform: "scale(1)",
+          },
+        },
       },
       animation: {
+        // challenge 2
         line1: "line1 0.7s ease-in-out forwards",
         line2: "line2 0.7s ease-in-out forwards",
         line3: "line3 0.7s ease-in-out forwards",
+
+        // challenge 3
         "sky-turns-black":
           "sky-turns-black 4s cubic-bezier(.4,0,.49,1) infinite",
         "sun-goes-down": "sun-goes-down 4s cubic-bezier(.4,0,.49,1) infinite",
@@ -62,6 +100,14 @@ export default {
         "shadow-on-the-floor":
           "shadow-on-the-floor 4s cubic-bezier(.4,0,.49,1) infinite",
         "fading-sand": "fading-sand 4s cubic-bezier(.4,0,.49,1) infinite",
+
+        // challenge 4
+        "jump-jump-1":
+          "jump-jump-1 2s cubic-bezier(.21,.98,.6,.99) infinite alternate",
+        "jump-jump-2":
+          "jump-jump-2 2s cubic-bezier(.21,.98,.6,.99) infinite alternate",
+        "jump-jump-3":
+          "jump-jump-3 2s cubic-bezier(.21,.98,.6,.99) infinite alternate",
       },
     },
   },
