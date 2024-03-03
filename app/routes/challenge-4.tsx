0216cfae13a382daa6a12c0ecd03b04535f1b62a
kensiiwasaki <kensiiwasaki@gmail.com>
2024-03-03 22:50:16 +0900

    pxに修正

--- app/routes/challenge-4.tsx
+++ app/routes/challenge-4.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const ChallengeComponents = () => {
   return (
     <div className="absolute top-1/2 left-1/2 w-[400px] h-[400px] mt-[-200px] ml-[-200px] rounded-[2px] shadow-[1px_2px_10px_0px_rgba(0,0,0,0.3)] bg-[#E56262] text-white">
-      <div className="absolute w-220 h-220 top-90 left-90">
-        <div className="absolute z-30 w-30 h-30 top-95 left-95 bg-white rounded-full animate-jump-jump-1"></div>
-        <div className="absolute z-20 w-60 h-60 top-80 left-80 bg-white rounded-full animate-jump-jump-2"></div>
-        <div className="absolute z-10 w-90 h-90 top-65 left-65 bg-white rounded-full animate-jump-jump-3"></div>
+      <div className="absolute w-[220px] h-[220px] top-[90px] left-[90px]">
+        <div className="absolute z-30 w-[30px] h-[30px] top-[95px] left-[95px] bg-white rounded-full animate-jump-jump-1"></div>
+        <div className="absolute z-20 w-[60px] h-[60px] top-[80px] left-[80px] bg-white rounded-full animate-jump-jump-2"></div>
+        <div className="absolute z-10 w-[90px] h-[90px] top-[65px] left-[65px] bg-white rounded-full animate-jump-jump-3"></div>
       </div>
     </div>
   );
